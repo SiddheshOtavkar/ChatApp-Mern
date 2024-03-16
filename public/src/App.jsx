41c26e19d0eefc4ea737b3073b4422c3bf3f82aa
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-16 13:30:36 +0530

    Basic Setup Done

---
+++ public/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Chat from './pages/Chat';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Chat />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
