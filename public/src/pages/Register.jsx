225a436747e952ef6fb409b6e57ee17f09f57357
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-16 21:58:36 +0530

    Registeration done

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoute";
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+
     const [values, setValues] = useState({
         username: "",
         email: "",
@@ -11,12 +26,58 @@ const Register = () => {
         confirmPassword: "",
     });
 
-    const handleFormSubmit = (e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
+        alert("form")
+        if (handleValidation()) {
+            const { email, username, password } = values;
+            const { data } = await axios.post(registerRoute, {
+                username,
+                email,
+                password,
+            });
+
+            if (data.success === false) {
+                toast.error(data.message, toastOptions);
+            }
+
+            if (data.success === true) {
+                toast.success(data.message, toastOptions);
+                localStorage.setItem(import.meta.env.VITE_APP_LOCALHOST_KEY, JSON.stringify(data.user));
+                navigate("/");
+            }
+        }
     };
 
-    const handleChange = () => {
-        console.log("Hello");
+    const handleValidation = () => {
+        const { username, email, password, confirmPassword } = values;
+        if (password != confirmPassword) {
+            toast.error(
+                `Password and confirm password must be same.`,
+                toastOptions,
+            );
+            return false;
+        }
+        else if (username.length < 3) {
+            toast.error(
+                `Username should be greater than 3 characters.`,
+                toastOptions,
+            );
+            return false;
+        }
+        else if (email === "") {
+            toast.error("Email is required.", toastOptions);
+            return false;
+        }
+
+        return true;
+    };
+
+    const handleChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
     };
 
     return (
@@ -51,12 +112,13 @@ const Register = () => {
                         name="confirmPassword"
                         onChange={(e) => handleChange(e)}
                     />
-                    <button>Create User</button>
+                    <button type="submit">Create User</button>
                     <span>
                         Already have a account ? <Link to={"/login"}>Login</Link>
                     </span>
                 </form>
             </FormContainer>
+            <ToastContainer />
         </>
     );
 };
