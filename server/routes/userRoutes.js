62bc8cba02616b03bcca61ca5950871785cbc6f5
SiddheshOtavkar <siddheshotavkar06@gmail.com>
2024-03-18 14:21:39 +0530

    chat-backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,11 +4,12 @@ const {
     registerController,
     loginController,
     setAvatarController,
+    getAllUsersController,
 } = require("../controllers/userController");
 
 router.post("/register", registerController);
 router.post("/login", loginController);
 router.post("/setavatar/:id", setAvatarController);
-router.get("/allusers/:id", getAllUsers);
+router.get("/allusers/:id", getAllUsersController);
 
 module.exports = router;
